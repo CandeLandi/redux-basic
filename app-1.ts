1f44a1054627ee55aecdb7188375c540e85f47cf
Candela Landi <landicandela01@gmail.com>
2024-06-19 10:15:12 -0300

    finish redux basic

--- app-1.ts
+++ app-1.ts
@@ -28,30 +28,3 @@ function reducer(state = 10, action: Action) {
   }
 }
 
-// Usar el reducer
-const incrementadorAction: Action = {
-  type: "INCREMENTAR",
-};
-
-console.log(reducer(10, incrementadorAction));
-
-const decrementadorAction: Action = {
-  type: "DECREMENTAR",
-};
-
-console.log(reducer(10, decrementadorAction));
-
-const multiplicarAction: Action = {
-  type: "MULTIPLICAR",
-  payload: 2,
-};
-
-console.log(reducer(10, multiplicarAction));
-
-const dividirAction: Action = {
-  type: "DIVIDIR",
-  payload: 2,
-};
-
-
-console.log(reducer(10, dividirAction));
